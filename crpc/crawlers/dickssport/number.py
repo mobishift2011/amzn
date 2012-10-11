9e2e2f0703a999d40b509e85289793808f6609d0
Yuande Liu <miraclecome@gmail.com>
2012-10-11 15:01:58 +0800

    delete useless file and change dickssport number.py file

--- crpc/crawlers/dickssport/number.py
+++ crpc/crawlers/dickssport/number.py
@@ -8,8 +8,8 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 
 con = pymongo.Connection('127.0.0.1')
-col = con['cabelas']['category']
-num = col.find({'is_leaf': True, 'num': {'$exists': True}}, fields=['cats', 'num'], timeout=False)
+col = con['dickssport']['category']
+num = col.find({'leaf': True, 'num': {'$exists': True}}, fields=['cats', 'num'], timeout=False)
 total = 0
 for i in num:
     total += int(i['num'])

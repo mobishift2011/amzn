a7a0ce354326af6e28be37f0e56f44f7ea927a8a
jingchaohu <jingchaohu@favbuy.com>
2012-10-16 15:40:45 +0800

    refactored settings

--- crpc/settings.py
+++ crpc/settings.py
@@ -2,25 +2,31 @@
 # -*- coding:utf-8 -*-
 import os
 import sys
+from itertools import chain
 
-CRPC_ROOT = os.path.abspath(os.path.dirname(__file__))
-sys.path.insert(0, CRPC_ROOT)
+envs = {
+    'COMMON': {
+        'CRPC_ROOT': os.path.abspath(os.path.dirname(__file__)),
+        'ENV_NAME': "crpc", 
+    },
+    'DEV': {
+        'PEERS': ['root@127.0.0.1'],
+        'USE_INDEX': ' --index-url http://e.pypi.python.org/simple/',
+    },
+    'TEST': {
+        'PEERS': [
+            'root@ec2-54-245-25-85.us-west-2.compute.amazonaws.com',
+            'root@ec2-54-245-31-157.us-west-2.compute.amazonaws.com',
+            'root@ec2-50-112-195-236.us-west-2.compute.amazonaws.com',
+            'root@ec2-54-245-15-250.us-west-2.compute.amazonaws.com',
+        ],
+        'USE_INDEX': ' --index-url http://b.pypi.python.org/simple/',
+    },
+}
 
-ENV_NAME = "crpc"
+env = os.environ.get("ENV")
+if not env:
+    env = "DEV"
 
-FETCHING_PEERS = [
-    'root@ec2-54-245-25-85.us-west-2.compute.amazonaws.com',
-    'root@ec2-54-245-31-157.us-west-2.compute.amazonaws.com',
-]
-
-PARSING_PEERS = [
-    'root@ec2-50-112-195-236.us-west-2.compute.amazonaws.com',
-    'root@ec2-54-245-15-250.us-west-2.compute.amazonaws.com',
-]
-
-PEERS = []
-PEERS.extend(FETCHING_PEERS)
-PEERS.extend(PARSING_PEERS)
-
-#PEERS = ['root@127.0.0.1']
-USE_INDEX = ' --index-url http://b.pypi.python.org/simple/'
+for key, value in chain(envs['COMMON'].iteritems(), envs[env].iteritems()):
+    globals()[key] = value

488d44c72c184815188420ab6afd60701ea8e2b2
observerss <jingchaohu@gmail.com>
2012-10-16 14:59:06 +0800

    added events

---
+++ crpc/crawlers/common/events.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from helpers.signals import Signal
+
+# usage:
+# product_updated.send(sender="amazon.parsepage", product = product, updated_fields = ['title',... ])
+product_updated = Signal("product_updated")
+
+product_deleted = Siganl("product_deleted")
+
+product_analysis_failed = Signal("asdfasdfasdf")

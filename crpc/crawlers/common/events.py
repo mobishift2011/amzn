3e2949f537449e9575d7d2083045fd2a585ef927
jingchaohu <jingchaohu@favbuy.com>
2012-10-16 20:20:17 +0800

    first design of events

--- crpc/crawlers/common/events.py
+++ crpc/crawlers/common/events.py
@@ -2,10 +2,37 @@
 # -*- coding: utf-8 -*-
 from helpers.signals import Signal
 
-# usage:
-# product_updated.send(sender="amazon.parsepage", product = product, updated_fields = ['title',... ])
-product_updated = Signal("product_updated")
+""" product_saved kwargs:
 
+site        =   site         (amazon, newegg, ...)
+key         =   key          
+is_new      =   False
+is_updated  =   False 
+
+"""
+product_saved = Signal("product_saved")
+
+
+""" product_failed kwargs:
+
+site    =   site         (amazon, newegg, ...)
+key     =   key          
+resaon  =   ...
+
+"""
+product_failed = Signal("product_failed")
+
+
+""" product_deleted kwargs:
+
+site    =   site         (amazon, newegg, ...)
+key     =   key          
+resaon  =   ...
+
+"""
 product_deleted = Signal("product_deleted")
 
-product_analysis_failed = Signal("asdfasdfasdf")
+
+category_saved = Signal("category_saved")
+category_failed = Signal("category_failed")
+category_deleted = Signal("category_deleted")

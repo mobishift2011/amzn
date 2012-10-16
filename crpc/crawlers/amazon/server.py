c1f30a3c5165aa302be72be88dd1dd346db330bb
jingchaohu <jingchaohu@favbuy.com>
2012-10-16 21:53:27 +0800

    a stash for amazon crawler

--- crpc/crawlers/amazon/server.py
+++ crpc/crawlers/amazon/server.py
@@ -10,9 +10,7 @@ import sys
 import time
 import redis
 import zerorpc
-import logging
 import requests
-import traceback
 import lxml.html
 
 from urllib import quote, unquote
@@ -21,17 +19,26 @@ from datetime import datetime, timedelta
 from models import *
 from settings import *
 
-s = requests.session(headers={'User-Agent':'Mozilla 5.0/b'})
+from crawlers.common.events import category_saved, category_failed, category_deleted
+from crawlers.common.events import product_saved, product_failed, product_deleted
+
     
 class Server:
-    def test(self):
-        return "this is a test"
+    def __init__(self):
+        self.s = requests.session(headers={'User-Agent':'Mozilla 5.0/b'})
+        self.site = "amazon"
+        
+    def crawl_category(self):
+        """ crawl all category info """
+        pass
 
     def crawl_listing(self, url):
+        """ crawl listing page """
         content = self.fetch_page(url)
         self.parse_listing(url, content)
         
     def crawl_product(self, url):
+        """ crawl product page """
         content = self.fetch_page(url)
         self.parse_product(url, content)
         
@@ -40,7 +47,7 @@ class Server:
         if r.status_code == 200:
             return r.content
         elif r.status_code == 404:
-            return ''
+            raise ValueError('page not found')
         else:
             raise ValueError('the crawler seems to be banned!!')
     
@@ -48,94 +55,98 @@ class Server:
         """ index info about each product -> db """
         t = lxml.html.fromstring(content)
         for block in t.xpath('//div[starts-with(@id,"result_")]'):
-            try:
-                link = block.xpath(".//a")[0].get("href")
-            except:
-                continue
-    
-            m = re.compile(r'http://www.amazon.com/(.*)/dp/(.*?)/').search(link)
-            if not m:
-                m = re.compile(r'http://www.amazon.com/(.*)/dp/(.*)').search(link)
-    
-            if m:
-                slug, asin = m.group(1), m.group(2)  
-    
-                is_new = False
-                p = Product.objects(asin=asin).first()
-                if not p:
-                    p = Product(asin=asin)
-                    is_new = True
-                    
-                # deal with multiple category
-                catn = url2catn(url)
-                if catn not in p.catns:
-                    p.catns.append(catn)
+            self._parse_block(block, url)
 
-                c = Category.objects(catn=catn).first()
-                if c:
-                    p.cats = c.cats
+    def _parse_block(self, block, url):
+        """ parse and save product info for a block of listing page """
+        try:
+            link = block.xpath(".//a")[0].get("href")
+        except:
+            continue
     
-                # some infomation we just update once and kept it forever
-                if is_new:
-                    p.slug = slug
-                    p.cover_image_url = block.xpath(".//img")[0].get('src')
-                    p.list_update_time = datetime.utcnow()
-                    p.updated = False
-    
-                # others we might want to update it more frequently
-                try:
-                    p.price = float(block.xpath('.//div[@class="newPrice"]')[0].xpath(".//span")[-1].text_content().strip()[1:].replace(',',''))
-                except:
-                    try:
-                        p.pricestr = block.xpath('.//div[@class="newPrice"]')[0].text_content().strip()
-                    except:
-                        pass
-    
+        m = re.compile(r'http://www.amazon.com/(.*)/dp/(.*?)/').search(link)
+        if not m:
+            m = re.compile(r'http://www.amazon.com/(.*)/dp/(.*)').search(link)
+   
+        if m:
+            slug, key = m.group(1), m.group(2)  
+  
+            is_new = False
+            p = Product.objects(key=key).first()
+            if not p:
+                p = Product(key=key)
+                is_new = True
+                
+            # deal with multiple category
+            catn = url2catn(url)
+            if catn not in p.catns:
+                p.catns.append(catn)
+
+            c = Category.objects(catn=catn).first()
+            if c:
+                p.cats = c.cats
+
+            # some infomation we just update once and kept it forever
+            if is_new:
+                p.slug = slug
+                p.cover_image_url = block.xpath(".//img")[0].get('src')
+                p.list_update_time = datetime.utcnow()
+                p.updated = False
+
+            # others we might want to update it more frequently
+            is_updated = False
+            try:
+                price = block.xpath('.//div[@class="newPrice"]')[0].xpath(".//span")[-1].text_content().strip()[1:].replace(',','')
+                if price != p.price:
+                    p.price = price
+                    is_updated = True
+            except:
+                pass
+
+            try:
                 p.save()
+            except Exception as e:
+                raise ValueError("validation failed")
+            else:
+                product_saved.send(sender = "amazon.parse_listing", 
+                                    site = self.site,
+                                    key = p.key,
+                                    is_new = is_new,                        
+                                    is_updated = is_updated)
     
     def parse_product(self, url, content):
         if url.endswith('/'):
-            asin = url.split('/')[-2] 
+            key = url.split('/')[-2] 
         else:
-            asin = url.split('/')[-1] 
+            key = url.split('/')[-1] 
 
         is_new = False
-        p = Product.objects(asin=asin).first()
+        p = Product.objects(key=key).first()
         if not p:
-            p = Product(asin=asin)
+            p = Product(key=key)
             is_new = True
-
-        # might not available
-        if not content:
-            try:
-                p.delete()
-            except:
-                pass
-            return
 
         t = lxml.html.fromstring(content)
 
         p.title = t.xpath('//*[@id="btAsinTitle"]')[0].text_content()
+        is_updated = False
     
         try:
-            p.manufactory = t.xpath('//h1[@class="parseasinTitle "]/following-sibling::*/a[@href]')[0].text_content().strip()
+            p.brand = t.xpath('//h1[@class="parseasinTitle "]/following-sibling::*/a[@href]')[0].text_content().strip()
             p.vartitle = t.xpath('//span[@id="variationProductTitle"]')[0].text_content().strip()
         except:
             pass
-            #traceback.print_exc()
     
         # like
         try:
-            p.stars = float(t.xpath('//*[@id="handleBuy"]/div[2]/span[1]/span/span/a/span')[0].get('title').split()[0])
-            p.reviews_count = int(t.xpath('//div[@class="jumpBar"]//span[@class="crAvgStars"]/a')[0].text_content().replace(',','').split()[0])
-            p.like = int(t.xpath('//span[@class="amazonLikeCount"]')[0].text_content().replace(',',''))
+            rating = t.xpath('//*[@id="handleBuy"]/div[2]/span[1]/span/span/a/span')[0].get('title').split()[0]
+            like = t.xpath('//span[@class="amazonLikeCount"]')[0].text_content().replace(',','')
+            num_reviews = t.xpath('//div[@class="jumpBar"]//span[@class="crAvgStars"]/a')[0].text_content().replace(',','').split()[0]
         except:
-            pass
-            #traceback.print_exc()
+            rating, like, num_reviews = "", "", ""
     
         # summary & model & salesrank
         try:
-    
             pd = t.xpath('//td[h2="Product Details"]//div[@class="content"]//ul')[0]
             summarys = []
             for l in pd.xpath(".//li"):
@@ -145,16 +156,29 @@ class Server:
                 summarys.append(lc.strip())
             p.summary = '\n'.join(summarys) 
             p.model = pd.xpath('li[contains(b, "model")]')[0].text_content().split()[-1]
-            p.salesrank = pd.xpath('li[@id="SalesRank"]')[0].text_content().replace('\n','').split(':',1)[1]
-            p.salesrank = re.sub('\(.*(}|\))', '', p.salesrank).strip()
+            sales_rank = pd.xpath('li[@id="SalesRank"]')[0].text_content().replace('\n','').split(':',1)[1]
+            sales_rank = re.sub('\(.*(}|\))', '', sales_rank).strip()
         except:
-            pass
-            #traceback.print_exc()
+            sales_rank = ""
     
         p.full_update_time = datetime.utcnow()
         p.updated = True
     
-        p.save()
+        for name in ['rating','like','num_reviews','sales_rank']:
+            if getattr(p, name) != locals()[name]:
+                setattr(p, name, locals()[name])
+                is_updated = True
+    
+        try:
+            p.save()
+        except Exception as e:
+            raise ValueError("validation failed")
+        else:
+            product_saved.send(sender = "amazon.parse_listing", 
+                                site = self.site,
+                                key = p.key,
+                                is_new = is_new,                        
+                                is_updated = is_updated)
 
 if __name__ == '__main__':
     server = zerorpc.Server(Server())

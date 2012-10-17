556009a04c97d96af99147befc6fd611cf6a4c6a
Yuande Liu <miraclecome@gmail.com>
2012-10-17 09:46:41 +0800

    add some events

--- crpc/crawlers/common/events.py
+++ crpc/crawlers/common/events.py
@@ -8,4 +8,12 @@ product_updated = Signal("product_updated")
 
 product_deleted = Signal("product_deleted")
 
-product_analysis_failed = Signal("asdfasdfasdf")
+product_analysis_failed = Signal("product_analysis_failed")
+
+product_created = Signal("product_created")
+
+page_parse_error = Signal("page_parse_error")
+
+page_download_failed = Signal("page_download_failed")
+
+debug_info =  Signal("debug_info")

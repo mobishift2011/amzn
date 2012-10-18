18f35ae51f10dad9d3b095707da423770e94087d
observerss <jingchaohu@gmail.com>
2012-10-18 13:29:01 +0800

    modified cleanser

--- crpc/backends/cleansing/cleanser.py
+++ crpc/backends/cleansing/cleanser.py
@@ -1,9 +1,63 @@
 #!/usr/bin/env python
 # -*- coding: utf-8
-from models import Product
+import re
+from models import Product, Model, Session, engine
+from sqlalchemy import func
+from datetime import datetime
+    
+goodcharacter = re.compile('^[0-9a-zA-Z-]+$')
+
+MODEL_BEST          = 0
+MODEL_DEFAULT       = -1
+MODEL_UNCLASSIFIED  = -999
+MODEL_REPEAT        = -888
+
+def extract_models():
+    blocksize = 10000
+    session = Session()
+    count = session.query(func.count(Product.id)).filter_by(level=MODEL_BEST).scalar()
+    models = set([])
+    for i in range(count/blocksize):
+        if i<30:
+            continue
+        #if i>=30:
+        #    break
+        print i
+        ms = set(( p.model.upper() for p in session.query(Product).filter_by(level=MODEL_BEST).offset(i*blocksize).limit(blocksize) ))
+        models |= ms
+   
+    print 'got exitings'
+    exist_models = set((m.model for m in session.query(Model))) 
+    print 'set minus'
+    models -= exist_models
+
+    print 'inserting new', len(models)
+    engine.execute("insert into model (model, updated_at) values (%s, %s)", [(m, datetime.utcnow()) for m in models])
+                
+def getlevel(model):
+    if goodcharacter.match(model):
+        return MODEL_BEST
+    else:
+        return MODEL_UNCLASSIFIED
 
 def cleanse():
-    pass
+    blocksize = 10000
+    session = Session()
+    count = session.query(func.count(Product.id)).filter_by(level=MODEL_DEFAULT).scalar()
+    for i in range(count/blocksize):
+        _part_cleanse(i*blocksize, blocksize)
+
+def _part_cleanse(offset, limit):
+    print offset, limit
+    session = Session()
+    for p in session.query(Product).filter_by(level=-1).offset(offset).limit(limit):
+        level = getlevel(p.model) 
+        if p.level != level:
+            print p.model, p.site, p.title, level
+            p.level = level
+            session.add(p)
+    session.commit()
 
 if __name__ == '__main__':
-    cleanse()
+    #cleanse()
+    extract_models()
--- crpc/backends/cleansing/models.py
+++ crpc/backends/cleansing/models.py
@@ -16,14 +16,25 @@ session = Session()
 
 
 class Product(Base):
-    __tablename__ = 'Product'
+    __tablename__ = 'product'
+
     id          =   Column(Integer, primary_key=True)
     model       =   Column(String(256), index=True)
     title       =   Column(String(1024))
     site        =   Column(String(32))
     key         =   Column(String(1024))
+    level       =   Column(Integer, default=-1, index=True)
     UniqueConstraint('site', 'key', name='sitekey')
 
 Index('product_site_key', Product.site, Product.key)
+
+class Model(Base):
+    __tablename__ = 'model'
+
+    id              =   Column(Integer, primary_key=True)
+    model           =   Column(String(128), unique=True, index=True)
+    global_volume   =   Column(Integer, index=True)
+    local_volume    =   Column(Integer, index=True)
+    updated_at      =   Column(DateTime, index=True)
 
 Base.metadata.create_all(engine)

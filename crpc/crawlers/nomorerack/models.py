5437e00ae3c3fda4b17e6935539494d0f8bf9805
TY@MacBook Pro <tianyu0915@gmail.com>
2012-10-30 15:48:29 +0800

    refs #79 start a new crawler `nomorerack.com`

---
+++ crpc/crawlers/nomorerack/models.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+crawlers.bluefly.models
+~~~~~~~~~~~~~~~~~~~~~~
+
+Implements Product and Category Model for Amazon
+"""
+DB = 'nomorerack-test'
+from settings import MONGODB_HOST
+from mongoengine import *
+connect(db=DB, alias=DB, host=MONGODB_HOST)
+from crawlers.common.models import BaseCategory, BaseProduct,BaseReview,LuxuryProduct,BaseEvent
+class Event(BaseEvent):
+    sale_id = StringField(unique=True)
+    meta        =   {
+        "db_alias": DB,
+    }
+
+class Category(BaseCategory):
+    """ we generates category by catn identifier """
+    key =   StringField(unique=True)
+    url = StringField()
+    
+    meta        =   {
+        "db_alias": DB,
+    }
+
+class Product(LuxuryProduct):
+    sale_id = StringField()
+    category_key = StringField()
+
+    url = StringField()
+    listprice = StringField()
+    return_policy  = StringField()
+    color = StringField()
+
+    def url(self):
+        return self.url
+
+    meta                =   {
+        "db_alias": DB,
+    }
+
+class  Review(BaseReview):
+    product_key = StringField()
+    meta                =   {
+        "db_alias": DB,
+    }

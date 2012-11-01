5e74ad45fd45c6ca6cf7b20cec5af30b0173a38f
TY@MacBook Pro <tianyu0915@gmail.com>
2012-11-01 12:59:17 +0800

    refs #38

--- crpc/crawlers/bluefly/models.py
+++ crpc/crawlers/bluefly/models.py
@@ -6,7 +6,7 @@ crawlers.bluefly.models
 
 Implements Product and Category Model for Amazon
 """
-DB = 'bluefly-test1'
+DB = 'bluefly-test2'
 from settings import MONGODB_HOST
 from mongoengine import *
 connect(db=DB, alias=DB, host=MONGODB_HOST)

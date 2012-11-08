e31652486349ba559674a1c06af49edc7a69ace7
jingchao <jingchaohu@favbuy.org>
2012-11-08 04:10:19 +0000

    display 10 results only

--- crpc/backends/webui/assets/js/controllers/DT_bootstrap.js
+++ crpc/backends/webui/assets/js/controllers/DT_bootstrap.js
@@ -155,7 +155,7 @@ $(document).ready(function() {
         "sDom": "<'row'<'span6'l><'span6'f>r>t<'row'<'span6'i><'span6'p>>",
         "sPaginationType": "bootstrap",
         "bAutoWidth": false,
-        "iDisplayLength": 50,
+        "iDisplayLength": 10,
         "oLanguage": {
             "sLengthMenu": "_MENU_ records per page"
         },

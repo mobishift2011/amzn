91bc80644cbe9b8ec31415ed8849ada01997ce39
bishop <yuandeliu@favbuy.com>
2012-11-14 21:24:53 +0800

    refs #86 change deploy_monitor

--- crpc/deploy_monitor.sh
+++ crpc/deploy_monitor.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 kill -9 $(pgrep -f run.p)
-kill -9 ${pgrep -f main.py}
+kill -9 $(pgrep -f main.py)
 . env.sh TEST
 dtach -n /tmp/monitormain.sock python backends/webui/main.py
 ulimit -n 4096 && dtach -n /tmp/monitorrun.sock python backends/monitor/run.py

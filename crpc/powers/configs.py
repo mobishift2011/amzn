f4f873a5be0ba7b61f3e6f55c76d77352f6f4c8c
ethan <ethan@msan.cn>
2012-11-16 14:11:11 +0800

    unlimit monitor views

--- crpc/powers/configs.py
+++ crpc/powers/configs.py
@@ -3,6 +3,7 @@ Created on 2012-11-6
 
 @author: ethan
 '''
+from os import listdir, path
 
 AWS_ACCESS_KEY = "AKIAIQC5UD4UWIJTBB2A"
 AWS_SECRET_KEY = "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"
@@ -10,6 +11,16 @@ IMAGE_S3_BUCKET = 'product.image.favbuy'
 IMAGE_ROOT = ''
 URL_EXPIRES_IN = 60 * 60 * 24 * 365 * 50
 
-DBS = (
+"""
+The same as the specific crawling name defined in the crawling folder.
+"""
+SITES = (
+    'bluefly',
     'gilt',
+    'hautelook',
+    'myhabit',
+    'nomorerack',
+    'onekingslane',
+    'ruelala',
+    'zulily',
 )

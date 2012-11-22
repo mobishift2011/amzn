c0828aaaf6e1199179a7cf77e48ce4e390b2ae89
bishop <yuandeliu@favbuy.com>
2012-11-22 20:16:36 +0800

    refs #86 fix ruelala bugs

--- crpc/crawlers/ruelala/server.py
+++ crpc/crawlers/ruelala/server.py
@@ -122,8 +122,8 @@ class Server(object):
             name = node.cssselect('td[style]:nth-of-type(1)')[0]
             sale_title = name.xpath('./a')[0].text_content()
             link = name.xpath('./a/@href')[0]
-            event_id = link.rsplit('/', 1)[0]
-            et_date = node.cssselect('td[style]:nth-of-type(2)')[0]
+            event_id = link.rsplit('/', 1)[-1]
+            et_date = node.cssselect('td[style]:nth-of-type(2)')[0].text_content().strip()
             time_str, time_zone = et_date.rsplit(' ', 1)
             events_begin = time_convert(time_str + ' ', '%m/%d at %I%p %Y', time_zone)
 

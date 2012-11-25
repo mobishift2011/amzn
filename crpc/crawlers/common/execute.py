5dbcc6dec969855bc2fe523fb481f0b17d861999
bishop <yuandeliu@favbuy.com>
2012-11-25 15:44:41 +0800

    refs #86 fix execute bug

--- crpc/crawlers/common/execute.py
+++ crpc/crawlers/common/execute.py
@@ -7,7 +7,7 @@ import crawllog -> from events import * -> from helpers.signals import Signal ->
     -> gevent.spawn a listener(from settings import *) -> listen from REDIS_HOST[SIGNALS]
     import crawllog # from routine import *, we already bind the signal, and have a gevent.spawned listener,
 """
-from rpcserver import CrawlerServer
+from crawlerserver import CrawlerServer
 from routine import *
 import sys
 import time

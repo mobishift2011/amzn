bc725db7fadd8e3472ac6d0134db9cf4544283f2
jingchao <jingchaohu@gmail.com>
2012-11-26 03:16:47 +0000

    change testfailure

--- crpc/tests/testfailure.py
+++ crpc/tests/testfailure.py
@@ -1,13 +1,19 @@
 import traceback
 from zerorpc import Client
-from settings import PEERS
+from settings import *
 
-for p in PEERS:
-    username, hostname = p.split('@')
-    print 'testing', hostname
-    c = Client('tcp://{hostname}:1234'.format(hostname=hostname))
-    try:
-        #c.call('test','123')
-        c.image('test')
-    except:
-        traceback.print_exc()
+def test(peers, port):
+    for p in peers:
+        username, hostname = p.split('@')
+        print 'testing', hostname
+        c = Client('tcp://{hostname}:{port}'.format(hostname=hostname, port=port))
+        try:
+            c.image('test')
+        except:
+            traceback.print_exc()
+
+
+if __name__ == '__main__':
+    test(CRAWLER_PEERS, CRAWLER_PORT)
+    test(POWER_PEERS, POWER_PORT)
+    

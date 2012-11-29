098c32a3c688e4b7a21285423f1a6f4ec048f90f
jingchao <jingchaohu@gmail.com>
2012-11-29 21:03:42 +0800

    added some dataset

---
+++ crpc/backends/matching/dataset/fetchdata.py
@@ -0,0 +1,35 @@
+from crawlers.myhabit.server2 import Server as Myhabit
+import os
+
+myhabit = Myhabit()
+
+def ensure_dir(f):
+    d = os.path.dirname(f)
+    if not os.path.exists(d):
+        os.makedirs(d)
+
+def do_fetch():
+    with open('from.txt') as f:
+        dept, subdept = '', ''
+        for l in f:
+            l = l.strip() 
+            if not l:
+                continue
+            if l.startswith('http://'):
+                url = l
+                name, content = myhabit.get_product_abstract_by_url(url)
+                fname = dept+'_'+subdept+'/'+name
+                ensure_dir(fname)
+                open(fname,'w').write(content.encode('utf-8'))
+                print fname
+                print content
+                print
+                print
+                print
+            elif l.startswith('?'):
+                subdept = l[1:].strip().replace(' ','_')
+            else:
+                dept = l.replace(' ','_')
+
+if __name__ == '__main__':
+    do_fetch()

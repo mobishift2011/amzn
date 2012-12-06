3b284f56083fc27327f4b27e2a254255b9537652
bishop <yuandeliu@favbuy.com>
2012-12-06 21:26:24 +0800

    refs #137 fix bug

--- crpc/powers/powerserver.py
+++ crpc/powers/powerserver.py
@@ -50,7 +50,7 @@ class PowerServer(object):
 
         #TO REMOVE
         import time
-        print site,' ' + doctype + ' ',  key + ' ', 'brand-<'+crawled_brand+'>  ',  'title-<'+ kwargs.get('title', ' ') +'>'+ ':'
+        # print site,' ' + doctype + ' ',  key + ' ', 'brand-<'+crawled_brand+'>  ',  'title-<'+ kwargs.get('title', ' ') +'>'+ ':'
         
         m = __import__('crawlers.'+site+'.models', fromlist=[doctype])
         extracter = Extracter()

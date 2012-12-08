8def007e280b0e64868f3f18c03f1339a3db7555
bishop <yuandeliu@favbuy.com>
2012-12-08 22:57:47 +0800

    add the autotask

---
+++ crpc/backends/monitor/autotask.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: bishop Liu <miracle (at) gmail.com>
+
+from gevent import monkey; monkey.patch_all()
+import os
+import random
+import pymongo
+import collections
+from datetime import datetime, timedelta
+
+from settings import MONGODB_HOST, CRPC_ROOT
+from .setting import UPCOMING_EVENTS_DETECT_INTERVAL, UPDATE_ALL_SITES_INTERVAL
+from crawlers.common.stash import exclude_crawlers
+from backends.monitor.scheduler import execute
+from backends.monitor.throttletask import task_already_running
+
+smethod_time = collections.defaultdict(set)
+conn = pymongo.Connection(host=MONGODB_HOST)
+
+def get_ordinary_crawlers():
+    """.. :py:method::
+        get ordinary crawlers from directory of CRPC_ROOT/crawlers/
+    """
+    crawlers = []
+    for crawler_name in os.listdir( os.path.join(CRPC_ROOT, 'crawlers') ):
+        path = os.path.join(CRPC_ROOT, 'crawlers', crawler_name)
+        if crawler_name not in exclude_crawlers and os.path.isdir(path):
+            crawlers.append(crawler_name)
+    return crawlers
+
+def detect_upcoming_new_schedule():
+    """.. :py:method::
+        upcoming to new event, schedule
+    """
+    dbs = conn.database_names()
+    for crawler_name in get_ordinary_crawlers():
+        if crawler_name in dbs:
+            collections = conn.crawler_name.collection_names()
+            if 'event' in collections:
+                upcoming_events = conn.crawler_name.event.find({'events_begin': {'$gte': datetime.utcnow()}}, fields=['events_begin'])
+                events_begin = set( [e['events_begin'] for e in upcoming_events] )
+                for begin in events_begin:
+                    smethod_time['{0}.new'.format(crawler_name)].add(begin)
+
+def arrange_update_schedule():
+    """.. :py:method::
+        update schedule
+    """
+    crawlers = get_ordinary_crawlers()
+    random.shuffle(crawlers)
+    one_interval = timedelta(seconds =  UPDATE_ALL_SITES_INTERVAL * 1.0 / len(crawlers) * 60)
+    _utcnow = datetime.utcnow()
+    for crawler in crawlers:
+        smethod_time['{0}.update'.format(crawler)].add(_utcnow)
+        _utcnow += one_interval
+
+def schedule_auto_new_task():
+    """.. :py:method::
+    """
+    while True:
+        detect_upcoming_new_schedule()
+        gevent.sleep(UPCOMING_EVENTS_DETECT_INTERVAL * 60)
+
+def schedule_auto_update_task():
+    """.. :py:method::
+    """
+    while True:
+        arrange_update_schedule()
+        gevent.sleep(UPDATE_ALL_SITES_INTERVAL * 60)
+
+def autotask():
+    """.. :py:method::
+        According to the order of demand, execute 'new' method first, 'update' maybe blocked for some turns.
+        This way looks like 'new' have higher priority than 'update'.
+
+        If 'new' or 'update' already running, all the expire task will try to execute, but failed,
+        then removed from the smethod_time. This can avoid too long set() in smethod_time.
+    """
+    _utcnow = datetine.utcnow()
+
+    #To find a sort way: if the list is already sorted, it do nearly nothing.
+    for k, v in smethod_time.iteritems():
+        site, method = k.split('.')
+        if method == 'new':
+            smethod_time[k] = set(sorted(v))
+            for new_time in smethod_time[k]:
+                if new_time <= _utcnow:
+                    # TODO new thrice
+                    execute(site, method)
+                    smethod_time[k].remove(new_time)
+                else: break
+
+    for k, v in smethod_time.iteritems():
+        site, method = k.split('.')
+        if method == 'update':
+            smethod_time[k] = set(sorted(smethod_time[k]))
+            for update_time in smethod_time[k]:
+                if update_time <= _utcnow:
+                    if not task_already_running(site, 'new'):
+                        execute(site, method)
+                        smethod_time[k].remove(update_time)
+                else: break
+
--- crpc/backends/monitor/scheduler.py
+++ crpc/backends/monitor/scheduler.py
@@ -6,24 +6,24 @@ Scheduler: runs crawlers in background
 """
 from gevent import monkey; monkey.patch_all()
 import gevent
-
-from backends.monitor.models import Schedule, Task
-from crawlers.common.routine import new, update, new_category, new_listing, new_product, update_category, update_listing, update_product
-
 import zerorpc
-from datetime import datetime, timedelta
-from settings import CRAWLER_PEERS, CRAWLER_PORT
-from throttletask import task_already_running, task_completed, task_broke_completed
 from functools import partial
+from datetime import datetime, timedelta
+
+from crawlers.common.routine import new, update, new_category, new_listing, new_product, update_category, update_listing, update_product
+from helpers.rpc import get_rpcs
+from settings import CRAWLER_PEERS, CRAWLER_PORT
+from backends.monitor.models import Schedule, Task
+from backends.monitor.throttletask import task_already_running, task_completed, task_broke_completed
+from backends.monitor.autotask import schedule_auto_new_task, schedule_auto_update_task, autotask
 from .setting import EXPIRE_MINUTES
 
-from helpers.rpc import get_rpcs
 
 def execute(site, method):
     """ execute CrawlerServer function
     """
     if not task_already_running(site, method):
-        gevent.spawn(globals()[method], site, get_rpcs(CRAWLER_PEERS, CRAWLER_PORT), 10) \
+        gevent.spawn(globals()[method], site, get_rpcs(CRAWLER_PEERS, CRAWLER_PORT), concurrency=10) \
                 .rawlink(partial(task_completed, site=site, method=method))
 
 
@@ -46,11 +46,16 @@ class Scheduler(object):
         return Schedule.objects(enabled=True) 
 
     def run(self):
+        gevent.spawn(schedule_auto_new_task)
+        gevent.spawn(schedule_auto_update_task)
+
         while True:
             try:
+                autotask()
                 for s in self.get_schedules():
                     if s.timematch():
                         execute(s.site, s.method)
+
                 # assume this for loop can be finished in less than one minute
                 gevent.sleep(60 - datetime.utcnow().second)
                 gevent.spawn(delete_expire_task)

b3cc00e289484f342020267db07f068a1567f363
ethan <ethan@msan.cn>
2012-12-10 15:11:35 +0800

    brand import script

---
+++ crpc/powers/script/schedule_insert.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from settings import CRPC_ROOT
+from configs import SITES
+from backends.monitor.models import Schedule
+from os import listdir
+import os
+
+"""
+A script tool to insert schedules for monitor.
+"""
+def insert():
+    excludes = ['common', 'ecost', 'bhphotovideo', 'bestbuy', 'dickssport', 'overstock', 'cabelas']
+    methods = ('new', 'new_category', 'new_listing', 'new_product', 'update', 'update_category', 'update_listing', 'update_product')
+    crontab_arguments = '*/30 * * * *'
+    for site in SITES:#listdir(os.path.join(CRPC_ROOT, "crawlers")):
+        for method in methods:
+            s = Schedule.objects(site=site, method=method)
+            if not s:
+                s = Schedule(site=site, method=method)
+                s.description = '{0} {1} test'.format(site , method)
+                s.minute, s.hour, s.dayofmonth, s.month, s.dayofweek = [ x for x in crontab_arguments.split(" ") if x ]
+                s.save()
+
+insert()

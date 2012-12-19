2bf11a704c06f81c937e5111bd7226828c4cfbb8
bishop <yuandeliu@favbuy.com>
2012-12-19 10:06:59 +0800

    fix an spelling error

--- crpc/backends/monitor/autoschedule.py
+++ crpc/backends/monitor/autoschedule.py
@@ -57,7 +57,7 @@ def auto_schedule():
         site, method = k.split('.')
         if method == 'new_thrice':
             for new_time in sorted(v):
-                if new_time + timedelta(mintues=1) <= _utcnow: # new need to plus 1 minute
+                if new_time + timedelta(minutes=1) <= _utcnow: # new need to plus 1 minute
                     execute(site, method)
                     smethod_time[k].remove(new_time)
                 else: break

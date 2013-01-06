055e052fd89c9719620a821c1e80451b6e66beab
bishop <yuandeliu@favbuy.com>
2013-01-06 11:41:51 +0800

    change crawler host

--- crpc/settings.py
+++ crpc/settings.py
@@ -61,30 +61,9 @@ envs = {
     },
     'TEST': {
         'CRAWLER_PEERS': [
-            {'host_string':'root@ec2-54-245-151-84.us-west-2.compute.amazonaws.com', 'port':1234},
-            {'host_string':'root@ec2-54-245-151-84.us-west-2.compute.amazonaws.com', 'port':1235},
-            {'host_string':'root@ec2-54-245-151-84.us-west-2.compute.amazonaws.com', 'port':1236},
-            {'host_string':'root@ec2-54-245-151-84.us-west-2.compute.amazonaws.com', 'port':1237},
-        ],
-        'POWER_PEERS': [
-            {'host_string':'root@ec2-54-245-149-120.us-west-2.compute.amazonaws.com', 'port':1244},
-            {'host_string':'root@ec2-54-245-149-120.us-west-2.compute.amazonaws.com', 'port':1245},
-            {'host_string':'root@ec2-54-245-149-120.us-west-2.compute.amazonaws.com', 'port':1246},
-            {'host_string':'root@ec2-54-245-149-120.us-west-2.compute.amazonaws.com', 'port':1247},
-        ],
-        'TEXT_PEERS': [
-            {'host_string':'root@ec2-54-245-151-84.us-west-2.compute.amazonaws.com', 'port':1238},
-        ],
-        'USE_INDEX': '',
-        'MONGODB_HOST': '10.252.14.6',
-        'REDIS_HOST': '10.252.14.6',
-    },
-    'TEST2': {
-        'CRAWLER_PEERS': [
             {'host_string':'root@ec2-50-112-200-82.us-west-2.compute.amazonaws.com', 'port':1234},
             {'host_string':'root@ec2-50-112-200-82.us-west-2.compute.amazonaws.com', 'port':1235},
             {'host_string':'root@ec2-50-112-200-82.us-west-2.compute.amazonaws.com', 'port':1236},
-            {'host_string':'root@ec2-54-245-144-50.us-west-2.compute.amazonaws.com', 'port':1237},
         ],
         'POWER_PEERS': [
             {'host_string':'root@ec2-54-245-149-120.us-west-2.compute.amazonaws.com', 'port':1244},
@@ -94,7 +73,6 @@ envs = {
         ],
         'TEXT_PEERS': [
             {'host_string':'root@ec2-50-112-200-82.us-west-2.compute.amazonaws.com', 'port':1238},
-            {'host_string':'root@ec2-54-245-144-50.us-west-2.compute.amazonaws.com', 'port':1239},
         ],
         'USE_INDEX': '',
         'MONGODB_HOST': '10.252.14.6',

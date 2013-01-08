64b7e7806ac80498e73eb3c49879be5b7f5179ea
bishop <yuandeliu@favbuy.com>
2013-01-08 22:45:02 +0800

    add buffer in logstat

--- crpc/backends/monitor/logstat.py
+++ crpc/backends/monitor/logstat.py
@@ -3,44 +3,33 @@
 """
     spawn one listener to listen crawlers' signal -- pre_general_update, post_general_update
 """
-from crawlers.common.events import *
+from gevent import monkey; monkey.patch_all()
+import gevent
+import collections
+import traceback
+from datetime import datetime, timedelta
+from gevent.coros import Semaphore
 
 from helpers.log import getlogger
-import traceback
+from crawlers.common.events import *
+from backends.monitor.models import Task, fail
+from backends.monitor.setting import EXPIRE_MINUTES
 
-from backends.monitor.models import Task, Fail, fail
-from datetime import datetime, timedelta
 
 logger = getlogger("crawlerlog")
+monitor_task = collections.defaultdict(dict)
+task_lock = Semaphore(1)
 
-def get_or_create_task(ctx):
-    """
-        Caution: This function should not update the status,
-        or else, post_general_update will be override by common_saved.
-    """
-    t = Task.objects(ctx=ctx).first()
-    if not t:
-        t = Task(ctx=ctx)
-        t.site, t.method, dummy = ctx.split('.')
-        t.started_at = datetime.utcnow()
-        t.save()
-#    else:
-#        t.update(set__status=Task.RUNNING, set__updated_at=datetime.utcnow())
-    return t
 
 @pre_general_update.bind('sync')
 def stat_pre_general_update(sender, **kwargs):
     site, method, dummy = sender.split('.')
-    try:
-        Task.objects(ctx=sender, status__ne=Task.FINISHED).update_one(set__site=site,
-                                            set__method=method,
-                                            set__status=Task.RUNNING,
-                                            set__started_at=datetime.utcnow(),
-                                            upsert=True)
-    except Exception as e:
-        logger.exception(e.message)
-        fail(site, method, kwargs.get('key',''), kwargs.get('url',''), traceback.format_exc())
-        
+    with task_lock:
+        if 'status' not in monitor_task[sender]:
+            monitor_task[sender].update( {'site': site,
+                                          'method': method,
+                                          'status': Task.RUNNING,
+                                          'started_at': datetime.utcnow()} )
 
 @post_general_update.bind('sync')
 def stat_post_general_update(sender, **kwargs):
@@ -49,83 +38,123 @@ def stat_post_general_update(sender, **kwargs):
     key = kwargs.get('key','')
     url = kwargs.get('url','')
     site, method, dummy = sender.split('.')
+    utcnow = datetime.utcnow()
 
-    try:
-        utcnow = datetime.utcnow()
+    with task_lock:
+        if 'started_at' not in monitor_task[sender]: # 'post-' before 'pre-'
+            monitor_task[sender].update({'started_at': utcnow})
+
         if not complete:
-            Task.objects(ctx=sender).update_one(set__site=site,
-                                                set__method=method,
-                                                set__status=Task.FAILED,
-                                                push__fails=fail(site, method, key, url, reason),
-                                                inc__num_fails=1,
-                                                set__updated_at=utcnow,
-                                                upsert=True)
-        else:
-            Task.objects(ctx=sender).update_one(set__site=site,
-                                                set__method=method,
-                                                set__status=Task.FINISHED,
-                                                set__updated_at=utcnow,
-                                                upsert=True)
-        Task.objects(ctx=sender, started_at__exists=False).update_one(set__started_at=utcnow)
-    except Exception as e:
-        logger.exception(e.message)
-        fail(site, method, key, url, traceback.format_exc())
+            if 'num_fails' in monitor_task[sender]:
+                monitor_task[sender]['num_fails'] += 1
+            else:
+                monitor_task[sender]['num_fails'] = 1
+            if 'fails' in monitor_task[sender]:
+                monitor_task[sender]['fails'].append( (site, method, key, url, reason, utcnow) )
+            else:
+                monitor_task[sender]['fails'] = [ (site, method, key, url, reason, utcnow) ]
 
+            monitor_task[sender].update({ 'site': site, 'method': method, 'status': Task.FAILED, 'ended_at': utcnow })
+        else:
+            monitor_task[sender].update({ 'site': site,
+                                          'method': method,
+                                          'status': Task.FINISHED,
+                                          'ended_at': utcnow, })
 
 @common_saved.bind('sync')
 def stat_save(sender, **kwargs):
-    logger.debug('{0} -> {1}'.format(sender,kwargs.items()))
     key = kwargs.get('key','')
     url = kwargs.get('url','')
     is_new = kwargs.get('is_new', False)
     is_updated = kwargs.get('is_updated', False)
-
     site, method, dummy = sender.split('.')
-    num_new = 1 if is_new else 0
-    num_update = 1 if is_updated else 0
-    num_finish = 1
+    utcnow = datetime.utcnow()
 
-    try:
-        Task.objects(ctx=sender).update_one(set__site=site,
-                                            set__method=method,
-                                            inc__num_new=num_new,
-                                            inc__num_update=num_update,
-                                            inc__num_finish=num_finish,
-                                            set__updated_at=datetime.utcnow(),
-                                            upsert=True)
-    except Exception as e:
-        logger.exception(e.message)
-        Task.objects(ctx=sender).update_one(set__site=site,
-                                            set__method=method,
-                                            inc__num_fails=1,
-                                            push__fails=fail(site, method, key, url, traceback.format_exc()),
-                                            set__updated_at=datetime.utcnow(),
-                                            upsert=True)
+    with task_lock:
+        if 'num_new' in monitor_task[sender]:
+            monitor_task[sender]['num_new'] += 1 if is_new else 0
+        else:
+            monitor_task[sender]['num_new'] = 1 if is_new else 0
+        if 'num_update' in monitor_task[sender]:
+            monitor_task[sender]['num_update'] += 1 if is_updated else 0
+        else:
+            monitor_task[sender]['num_update'] = 1 if is_updated else 0
+        if 'num_finish' in monitor_task[sender]:
+            monitor_task[sender]['num_finish'] += 1
+        else:
+            monitor_task[sender]['num_finish'] = 1
+
+        monitor_task[sender].update({ 'site': site,
+                                      'method': method,
+                                      'updated_at': utcnow, })
+#        Task.objects(ctx=sender).update_one(set__site=site,
+#                                            set__method=method,
+#                                            inc__num_fails=1,
+#                                            push__fails=fail(site, method, key, url, traceback.format_exc()),
+#                                            set__updated_at=datetime.utcnow(),
+#                                            upsert=True)
 
 
 @common_failed.bind('sync')
 def stat_failed(sender, **kwargs):
-    logger.error('{0} -> {1}'.format(sender,kwargs.items()))
     key  = kwargs.get('key', '')
     url  = kwargs.get('url', '')
     reason = repr(kwargs.get('reason', 'undefined'))
     site, method, dummy = sender.split('.')
+    utcnow = datetime.utcnow()
 
-    try:
-        Task.objects(ctx=sender).update_one(set__site=site,
-                                            set__method=method,
-                                            push__fails=fail(site, method, key, url, reason),
-                                            inc__num_fails=1,
-                                            set__updated_at=datetime.utcnow(),
-                                            upsert=True)
-    except Exception as e:
-        logger.exception(e.message)
-        Task.objects(ctx=sender).update_one(set__site=site,
-                                            set__method=method,
-                                            push__fails=fail(site, method, key, url, traceback.format_exc()),
-                                            inc__num_fails=1,
-                                            set__updated_at=datetime.utcnow(),
-                                            upsert=True)
+    with task_lock:
+        if 'num_fails' in monitor_task[sender]:
+            monitor_task[sender]['num_fails'] += 1
+        else:
+            monitor_task[sender]['num_fails'] = 1
+        if 'fails' in monitor_task[sender]:
+            monitor_task[sender]['fails'].append( (site, method, key, url, reason, utcnow) )
+        else:
+            monitor_task[sender]['fails'] = [ (site, method, key, url, reason, utcnow) ]
+
+        monitor_task[sender].update({ 'site': site,
+                                      'method': method,
+                                      'updated_at': utcnow, })
+
+def dump_monitor_task_to_db():
+    """.. :py:method::
+        dump monitor_task to mongodb,
+        delete task which are finished.
+        delete task which are expired.
+    """
+    utcnow = datetime.utcnow()
+    pop_keys = []
+
+    with task_lock:
+        for sender, task in monitor_task.iteritems():
+
+            # add finished task that can be delete from monitor_task in Memory
+            if 'ended_at' in task: # if bind is not 'sync': and 'updated_at' in task and task['ended_at'] - task['updated_at'] > timedelta(minutes=5):
+                pop_keys.append(sender)
+            elif utcnow - task['updated_at'] >= timedelta(minutes=EXPIRE_MINUTES):
+                task.update({'ended_at': utcnow})
+                pop_keys.append(sender)
+            else:
+                pass
+
+            # dump Memory to mongodb
+            Task.objects(ctx=sender).update_one(set__site=site,
+                                                set__method=method,
+                                                set__status=task['status'] if 'status' in task else None,
+                                                set__started_at=task['started_at'] if 'started_at' in task else utcnow,
+                                                set__updated_at=task['updated_at'] if 'updated_at' in task else utcnow,
+                                                set__ended_at=task['ended_at'] if 'ended_at' in task else None,
+                                                set__num_finish=task['num_finish'] if 'num_finish' in task else 0,
+                                                set__num_update=task['num_update'] if 'num_update' in task else 0,
+                                                set__num_new=task['num_new'] if 'num_new' in task else 0,
+                                                set__num_fails=task['num_fails'] if 'num_fails' in task else 0,
+                                                set__fails=[fail(*i) for i in task['fails']] if 'fails' in task else [],
+                                                upsert=True)
+
+        # delete finished task in monitor_task
+        for key in pop_keys: monitor_task.pop(key)
+        
 
 if __name__ == '__main__':
     print 'logstat'

57fd855108d4239f573c7b433b8ac78f6485a048
Yuande Liu <miraclecome@gmail.com>
2013-01-11 21:25:35 +0800

    change check_task

--- crpc/backends/monitor/check_task.py
+++ crpc/backends/monitor/check_task.py
@@ -13,11 +13,15 @@ task = conn['monitor']['task']
 
 def get_all_task(site):
     for obj in task.find({'site': site}, fields=['method', 'status', 'started_at', 'updated_at', 'ended_at', 'num_finish', 'num_update', 'num_new', 'num_fails']).sort('started_at', pymongo.DESCENDING):
-        print '{0}.{1} {2} {3} {4} {5}'.format(site, obj['method'], obj['status'], obj['started_at'], obj['ended_at'], obj['num_finish'])
+        print '{0}.{1} | {2} | {3} | {4} |\t {5} | {6} | {7} | {8} |'.format(
+                site, obj['method'], obj['status'], obj['started_at'].isoformat()[:-7],
+                obj['ended_at'].isoformat()[:-7], obj['num_finish'], obj['num_update'],
+                obj['num_new'], obj['num_fails'])
 
 if __name__ == '__main__':
-    if sys.argv[1]:
-        get_all_task(sys.argv[1])
+    if len(sys.argv) > 1:
+        for site in sys.argv[1:]:
+            get_all_task(site)
     else:
         for site in picked_crawlers:
             get_all_task(site)

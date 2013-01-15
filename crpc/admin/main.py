0cbca30f1f0eaed8a8871a84aad4069fb76c4110
jingchao <jingchaohu@gmail.com>
2013-01-15 12:01:33 +0800

    added auth

--- crpc/admin/main.py
+++ crpc/admin/main.py
@@ -29,33 +29,65 @@ class BaseHandler(tornado.web.RequestHandler):
     def __init__(self, *args, **kwargs): 
         tornado.web.RequestHandler.__init__( self, *args, **kwargs ) 
         self.jinja2_env = self.settings.get("jinja2_env") 
+    
+    def get_current_user(self):
+        user = self.get_secure_cookie('user')
+        return user
 
     def render_string(self, template_name, **kwargs): 
         # if the jinja2_env is present, then use jinja2 to render templates: 
         if self.jinja2_env: 
+            context = {
+                'current_user': self.get_current_user,
+                'xsrf_form_html': self.xsrf_form_html,
+            }
+            kwargs.update(context)
             return self.jinja2_env.get_template(template_name).render(**kwargs)
         else:
             return tornado.web.RequestHandler.render_string(self, template_name, **kwargs) 
 
 class IndexHandler(BaseHandler):
+    @tornado.web.authenticated
     def get(self):
         self.render("index.html")
 
 class ExampleHandler(BaseHandler):
+    @tornado.web.authenticated
     def get(self, module):
         self.render('examples/'+module+'.html')
+
+class LoginHandler(BaseHandler):
+    def get(self):
+        self.render('login.html')
+
+    def post(self):
+        username = self.get_argument('username')
+        password = self.get_argument('password')
+        if username == 'favbuy' and password == 'tempfavbuy':
+            self.set_secure_cookie('user', username)
+        next_url = self.get_argument('next', '/')
+        self.redirect(next_url)
+
+class LogoutHandler(BaseHandler):
+    @tornado.web.authenticated
+    def get(self):
+        self.clear_cookie('user')
+        next_url = self.get_argument('next', '/')
+        self.redirect(next_url)
 
 settings = {
     "static_path": STATIC_PATH,
     "template_path": TEMPLATE_PATH,
     "cookie_secret": "637d1f5c6e6d1be22ed907eb3d223d858ca396d8",
     "jinja2_env": JINJA2_ENV,
-    "login_url": "/login",
+    "login_url": "/login/",
     "xsrf_cookies": True,
 }
 
 application = tornado.web.Application([
-    (r"/(ui|form|chart|typography|gallery|table|calendar|grid|file-manager|tour|icon|error|login)/", ExampleHandler),
+    (r"/examples/(ui|form|chart|typography|gallery|table|calendar|grid|file-manager|tour|icon|error|login)/", ExampleHandler),
+    (r"/login/", LoginHandler),
+    (r"/logout/", LogoutHandler),
     (r"/", IndexHandler),
     (r"/assets/(.*)", tornado.web.StaticFileHandler, dict(path=settings['static_path'])),
 ], **settings)

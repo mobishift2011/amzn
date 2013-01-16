96eb42980da9a7e9e8ed4dd9f92020107ff985b2
jingchao <jingchaohu@gmail.com>
2013-01-16 19:17:52 +0800

    some result

--- crpc/admin/main.py
+++ crpc/admin/main.py
@@ -1,11 +1,54 @@
-import os
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import tornado.ioloop
 import tornado.web
 import tornado.httpserver
+import tornado.autoreload
+
+import jinja2 
+
 from webassets import Environment as AssetsEnvironment, loaders
 from webassets.ext.jinja2 import AssetsExtension
-import jinja2 
+
+import os
+import json
+from math import ceil
 from slumber import API
+from datetime import datetime, timedelta
+
+def get_site_module(site):
+    return __import__('crawlers.'+site+'.models', fromlist=['Category', 'Event', 'Product'])
+
+class Pagination(object):
+    def __init__(self, page, per_page, total_count):
+        self.page = page
+        self.per_page = per_page
+        self.total_count = total_count
+
+    @property
+    def pages(self):
+        return int(ceil(self.total_count / float(self.per_page)))
+
+    @property
+    def has_prev(self):
+        return self.page > 1
+
+    @property
+    def has_next(self):
+        return self.page < self.pages
+
+    def iter_pages(self, left_edge=2, left_current=2,
+                   right_current=5, right_edge=2):
+        last = 0
+        for num in xrange(1, self.pages + 1):
+            if num <= left_edge or \
+               (num > self.page - left_current - 1 and \
+                num < self.page + right_current) or \
+               num > self.pages - right_edge:
+                if last + 1 != num:
+                    yield None
+                yield num
+                last = num
 
 class Jinja2Environment(jinja2.Environment): 
     def load(self, template_path): 
@@ -36,12 +79,19 @@ def imagesize(imageobj, wxh):
             if h == 0 and w == size[0]:
                 h = size[1]
         return imageobj['url'] + '_{0}x{1}'.format(w, h)
+
 JINJA2_ENV.filters['imagesize'] = imagesize
 
 class BaseHandler(tornado.web.RequestHandler): 
     def __init__(self, *args, **kwargs): 
         tornado.web.RequestHandler.__init__( self, *args, **kwargs ) 
         self.jinja2_env = self.settings.get("jinja2_env") 
+
+    def prepare(self):
+        if self.request.headers.get("Content-Type") == "application/json":
+            self.json_args = json.loads(self.request.body)
+        else:
+            self.json_args = None
     
     def get_current_user(self):
         user = self.get_secure_cookie('user')
@@ -94,12 +144,106 @@ class ViewDataHandler(BaseHandler):
         if not subpath:
             self.redirect('/viewdata/events')
         if subpath == 'events':
-            result = api.event.get(offset=0, limit=20)
-            meta = result['meta']
-            events = result['objects']
-            self.render('viewdata/events.html', meta=meta, events=events)
+            self.render_events()
+        elif subpath == 'products':
+            self.render_products()
+        elif subpath == 'classification':
+            self.render_classification()
+
+    def render_classification(self):
+        from backends.matching.feature import sites
+        from itertools import chain
+        current_site = self.get_argument('site', sites[0])
+        offset = self.get_argument('offset', '0')
+        limit = self.get_argument('limit', '80')
+        offset, limit = int(offset), int(limit)
+
+        m = get_site_module(current_site)
+        events, categories = [], []
+        num_events = 0
+        num_categories = 0
+        if hasattr(m, 'Event'):
+            events = m.Event.objects()
+            num_events = len(events)
+            if num_events <= offset:
+                events = []
+                offset -= num_events
+            elif num_events >= offset+limit:
+                events = events[offset:offset+limit]
+                limit  = 0
+            else:
+                events = events[offset:]
+                offset = 0
+                limit -= (num_events - offset)
+        if hasattr(m, 'Category'):
+            categories = m.Category.objects()
+            num_categories = len(categories)
+            if limit == 0:
+                categories = []
+            else:
+                categories = categories[offset:offset+limit]
+        object_list = chain(events, categories)
+
+        total_count = num_events + num_categories
+        pagination = Pagination(offset/80+1, 80, total_count)
+
+        self.render('viewdata/classification.html', sites=sites, current_site=current_site,
+            object_list=object_list, pagination=pagination)
+
+    def render_products(self):
+        from backends.matching.feature import sites
+        kwargs = {}
+        for k, v in self.request.arguments.iteritems():
+            kwargs[k] = v[0]
+
+        offset = kwargs.get('offset', '0')
+        limit = kwargs.get('limit', '20')
+        kwargs['offset'] = int(offset)
+        kwargs['limit'] = int(limit)
+
+        result = api.product.get(**kwargs)
+        meta = result['meta']
+        products = result['objects']
+        sites = ['ALL'] + sites
+        times = {
+           'onehourago': datetime.utcnow()-timedelta(hours=1),
+           'onedayago': datetime.utcnow()-timedelta(days=1),
+           'oneweekago': datetime.utcnow()-timedelta(days=7),
+        }
+
+        pagination = Pagination(int(offset)/20+1, 20, meta['total_count'])
+
+        self.render('viewdata/products.html', meta=meta, products=products, sites=sites, 
+            times=times, pagination=pagination)
+
+    def render_events(self):
+        from backends.matching.feature import sites
+        kwargs = {}
+        for k, v in self.request.arguments.iteritems():
+            kwargs[k] = v[0]
+
+        offset = kwargs.get('offset', '0')
+        limit = kwargs.get('limit', '20')
+        kwargs['offset'] = int(offset)
+        kwargs['limit'] = int(limit)
+
+        result = api.event.get(**kwargs)
+        meta = result['meta']
+        events = result['objects']
+        sites = ['ALL'] + sites
+        times = {
+           'onehourago': datetime.utcnow()-timedelta(hours=1),
+           'onedayago': datetime.utcnow()-timedelta(days=1),
+           'oneweekago': datetime.utcnow()-timedelta(days=7),
+        }
+
+        pagination = Pagination(int(offset)/20+1, 20, meta['total_count'])
+
+        self.render('viewdata/events.html', meta=meta, events=events, sites=sites, 
+            times=times, pagination=pagination)
 
 settings = {
+    "debug": True,
     "static_path": STATIC_PATH,
     "template_path": TEMPLATE_PATH,
     "cookie_secret": "637d1f5c6e6d1be22ed907eb3d223d858ca396d8",
@@ -121,5 +265,6 @@ if __name__ == "__main__":
     server = tornado.httpserver.HTTPServer(application)
     server.bind(8888)
     server.start(1)
-    tornado.ioloop.IOLoop.instance().start()
-
+    ioloop = tornado.ioloop.IOLoop.instance()
+    tornado.autoreload.start(ioloop)
+    ioloop.start()

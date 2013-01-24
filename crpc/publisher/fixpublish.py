e63b59468d0d9abe7b30382f1a5f583c8acaf40d
Ye Li <yester.gmail.com>
2013-01-24 02:34:36 +0000

    added dryrun support.

--- crpc/publisher/fixpublish.py
+++ crpc/publisher/fixpublish.py
@@ -26,14 +26,16 @@ class DupeFixer:
     def get_module(self, site):
         return self.m[site]
     
-    def fix_product(self, site, key):
+    def fix_product(self, site, key, dryrun=False):
         m = self.get_module(site)
         result = self.mapi.product.get(site_key=site+"_"+key)
         for prod in result['objects']:
             try:
-                print prod['updated_at']
+                print "id={} updated_at={}, uri={}".format(prod['id'], prod['updated_at'], prod['resource_uri'])
+                if dryrun: continue
                 prodobj = m.Product.objects.get(key=key)
                 prodobj.publish_time = dateutil.parser.parse(prod['updated_at'])
+                prodobj.muri = prod['resource_uri']
                 prodobj.save()
                 print "site:{}, product:{} publish_time restored".format(site, key)
             except Exception as e:
@@ -45,8 +47,10 @@ class DupeFixer:
         result = self.mapi.event.get(site_key=site+"_"+key)
         for ev in result['objects']:
             try:
+                print "id={} updated_at={}, uri={}".format(ev['id'], ev['updated_at'], ev['resource_uri'])
                 evobj = m.Event.objects.get(event_id=key)
                 evobj.publish_time = dateutil.parser.parse(ev['updated_at'])
+                evobj.muri = ev['resource_uri']
                 evobj.save()
                 print "site:{}, event:{} publish_time restored".format(site, key)
             except Exception as e:
@@ -54,7 +58,7 @@ class DupeFixer:
                 print "site:{}, event:{} publish_time restore failed".format(site, key)
 
     def fix_products_in_file(self, filename):
-        pattern = re.compile('publishing product (?P<site>\w+):(?P<key>\w+) failed')
+        pattern = re.compile('publishing product (?P<site>[a-zA-Z0-9_\-]\w+):(?P<key>[a-zA-Z0-9_\-]+) failed')
         with open(filename) as f:
             for line in f:
                 m = pattern.search(line)
@@ -62,7 +66,7 @@ class DupeFixer:
                     self.fix_product(m.group('site'), m.group('key'))
 
     def fix_events_in_file(self, filename):
-        pattern = re.compile('publishing event (?P<site>\w+):(?P<key>\w+) failed')
+        pattern = re.compile('publishing event (?P<site>[a-zA-Z0-9_\-]\w+):(?P<key>[a-zA-Z0-9_\-]+) failed')
         with open(filename) as f:
             for line in f:
                 m = pattern.search(line)
@@ -79,6 +83,7 @@ if __name__ == '__main__':
     parser.add_option('-s', '--site', dest='site', help='site info', default='')
     parser.add_option('-e', '--ev', dest='ev', help='event id', default='')
     parser.add_option('-p', '--prod', dest='prod', help='product id', default='')
+    parser.add_option('-d', '--dryrun', dest='dryrun', action="store_true", help='dryrun', default=False)
 
     if len(sys.argv)==1:
         parser.print_help()
@@ -90,6 +95,6 @@ if __name__ == '__main__':
         fixer.fix_products_in_file(options.file)
         fixer.fix_events_in_file(options.file)
     elif options.site and options.prod:
-        fixer.fix_product(options.site, options.prod)
+        fixer.fix_product(options.site, options.prod, options.dryrun)
     elif options.site and options.ev:
-        fixer.fix_event(options.site, options.ev)
+        fixer.fix_event(options.site, options.ev, options.dryrun)

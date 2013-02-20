591a94cec95cb4efa53f928aa65ad2bea0aaa3e7
bishop <yuandeliu@favbuy.com>
2013-02-20 11:05:27 +0800

    add check events begin end

--- crpc/tests/publisher/check_event_begin_end.py
+++ crpc/tests/publisher/check_event_begin_end.py
@@ -28,5 +28,22 @@ def check_events_begin_end(data=collections.defaultdict(dict)):
         if data[site][key][1] != e['ends_at']:
             print 'events_end error: {0}, {1}'.format(site, key)
 
+def sync_time_mastiff_to_mongodb(data=collections.defaultdict(dict)):
+    ev = conn_m.event.find({}, fields=['site_key', 'starts_at', 'ends_at'])
+    for e in ev:
+        site, key = e['site_key'].split('_')
+        if site not in data: data[site] = {}
+        data[site][key] = [e['starts_at'], e['ends_at']]
+
+    for site in picked_crawlers:
+        col = conn[site].collection_names()
+        if 'event' in col:
+            ev = conn[site].event.find({}, fields=['event_id', 'events_begin', 'events_end'])
+            for e in ev:
+                e['events_begin'] = data[site][e['event_id']][0]
+                e['events_end'] = data[site][e['event_id']][1]
+                conn[site].event.save(e)
+
+
 if __name__ == '__main__':
-    pass
+    check_events_begin_end()

1a9d7d9925cc603aa0e299ab152d317d16ed4bb1
bishop <yuandeliu@favbuy.com>
2013-02-26 18:34:26 +0800

    add prove codeg

--- crpc/crawlers/common/prove_data.py
+++ crpc/crawlers/common/prove_data.py
@@ -6,12 +6,12 @@ from datetime import datetime
 from crawlers.common.stash import picked_crawlers
 
 fetch_page_site = ['bluefly']
-req_site = ['nomorerack', 'modnique']
-req_site.extend(fetch_page_site)
-
-login_site = ['beyondtherack', 'belleandclive', 'ideeli', 'lot18', 'onekingslane', 'ruelala', 'totsy', 'venteprivee', 'zulily']
-not_nedd_login = ['gilt', ]
 cookie_site = ['myhabit', 'hautelook']
+req_site = ['nomorerack', 'modnique']
+req_site.extend(cookie_site)
+
+login_site = ['beyondtherack', 'belleandclive', 'ideeli', 'lot18', 'totsy', 'venteprivee']
+not_need_login = ['gilt', 'onekingslane', 'ruelala', 'zulily' ]
 
 def get_site_module(site):
 
@@ -25,11 +25,22 @@ def get_site_module(site):
     return get_site_module.mod[site]
 
 for crawler in picked_crawlers:
+    old_exists, new_not_exist = 0
+    if crawler in req_site:
+        login = __import__('crawlers.{0}.server'.format(crawler), fromlist=['req'])
+    elif crawler in fetch_page_site:
+        login = requests.Session()
+    elif crawler in not_need_login:
+        login = requests.Session()
+    elif crawler in login_site:
+        login = __import__('crawlers.{0}.server'.format(crawler), fromlist=['{0}Login'.format(crawler)])
     module = get_site_module(crawler)
     for prd in module.Product.objects(products_end__lte=datetime.utcnow()):
         # link not exist, or exist link statistics
-        
-        __import__('crawlers.{0}.server'.format(crawler), fromlist=[])
-        prd['combine_url']
+        ret = login.get(prd['combine_url'])
+        if ret.status_code == 200:
+            old_exists += 1
     for prd in module.Product.objects(products_end__gt=datetime.utcnow()):
-        prd['combine_url']
+        ret = login.get( prd['combine_url'] )
+        if ret.status_code != 200:
+            new_not_exist += 1

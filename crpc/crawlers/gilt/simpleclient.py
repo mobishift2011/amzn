018a8366b5eb53661a1ae3ea6d5e642fbfb08c20
bishop <yuandeliu@favbuy.com>
2013-03-07 16:51:56 +0800

    fix error of simpleclient

--- crpc/crawlers/gilt/simpleclient.py
+++ crpc/crawlers/gilt/simpleclient.py
@@ -39,20 +39,23 @@ class CheckServer(object):
             node = tree.cssselect('div.content-container div.positions div.elements-container article.product-full section.product-details')[0]
             brand = node.cssselect('h3.product-brand')[0].text_content()
             title = node.cssselect('h1.product-name')[0].text_content()
-            price = node.cssselect('div.product-price div.gilt-price')[0].text_content().replace('$', '').replace('Gilt', '').strip()
             soldout = True if node.cssselect('form.sku-selection div.actions p.secondary-action a') else False
 
             listprice = node.cssselect('div.product-price div.original-price')
             listprice = listprice[0].text_content().replace('$', '').strip() if listprice else ''
             if listprice and '-' in listprice:
-                print 'gilt product[{0}] listprice {1} vs {2}'.format(url, listprice, prd.listprice)
+                print 'gilt product[{0}] listprice {1} vs {2}'.format(url, prd.listprice, listprice)
             elif listprice and float(prd.listprice.replace('$', '').strip()) != float(listprice):
                 print 'gilt product[{0}] listprice error: [{1}, {2}]'.format(url, prd.listprice.replace('$', '').strip(), listprice)
 
+            price = node.cssselect('div.product-price div.gilt-price')[0].text_content().replace('$', '').replace('Gilt', '').strip()
+            if '-' in price:
+                print 'gilt product[{0}] price {1} vs {2}'.format(url, prd.price, price)
+            elif float(prd.price.replace('$', '').replace('Gilt', '').strip()) != float(price):
+                print 'gilt product[{0}] price error: [{1}, {2}]'.format(url, prd.price.replace('$', '').replace('Gilt', '').strip(), price)
+
             if prd.title.lower() != title.lower():
                 print 'gilt product[{0}] title error: [{1}, {2}]'.format(url, prd.title.encode('utf-8').lower(), title.encode('utf-8').lower())
-            if float(prd.price.replace('$', '').replace('Gilt', '').strip()) != float(price):
-                print 'gilt product[{0}] price error: [{1}, {2}]'.format(url, prd.price.replace('$', '').replace('Gilt', '').strip(), price)
             if prd.soldout != soldout:
                 print 'gilt product[{0}] soldout error: [{1}, {2}]'.format(url, prd.soldout, soldout)
 

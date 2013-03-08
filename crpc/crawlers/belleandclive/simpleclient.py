316fad268d6da03e2347370cb27b9f0d3f6afc67
bishop <yuandeliu@favbuy.com>
2013-03-08 11:19:41 +0800

    add belleandclive

---
+++ crpc/crawlers/belleandclive/simpleclient.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: bishop Liu <miracle (at) gmail.com>
+
+import requests
+import lxml.html
+
+class CheckServer(object):
+    def __init__(self):
+        pass
+
+    def check_onsale_product(self, id, url):
+        pass

db8205a9c2390b8b1ca1bbe2f5561e3347ea6944
ethan <ethan@favbuy.com>
2013-03-21 12:05:04 +0800

    full dept crawling for 6pm, nordstrom, macys

---
+++ crpc/crawlers/nordstrom/server.py
@@ -0,0 +1,374 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: Ethan <ethan@favbuy.com>
+
+"""
+crawlers.nordstrom.server
+~~~~~~~~~~~~~~~~~~~
+
+This is the server part of zeroRPC module. Call by client automatically, run on many differen ec2 instances.
+"""
+from crawlers.common.stash import *
+from crawlers.common.events import common_saved, common_failed
+from models import Category, Product
+from deals.picks import Picker
+import requests
+import lxml.html
+import traceback
+from datetime import datetime
+import re
+
+HOST = 'http://shop.nordstrom.com'
+header = {
+    'Host': HOST,
+    'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:16.0) Gecko/20100101 Firefox/16.0',
+}
+req = requests.Session(prefetch=True, timeout=25, config=config, headers=header)
+
+class Server(object):
+    def __init__(self):
+        pass
+
+    def crawl_listing_of_no_leaf(self, page_node, ctx='', **kwargs):
+        href_set = set()
+        href_nodes = page_node.cssselect('div.main-content-right a')
+        for href_node in href_nodes:
+            href = href_node.get('href') or ''
+            if '/c/' in href:
+                match = re.search(r'https?://.+', href)
+                if not match:
+                    href = 'http://shop.nordstrom.com%s' % (href)
+                href_set.add(href)
+        for url in href_set:
+            print url
+            self.crawl_listing(url=url, ctx=ctx, **kwargs)
+
+    def crawl_category(self, ctx='', **kwargs):
+        res = requests.get(HOST)
+        res.raise_for_status()
+        tree = lxml.html.fromstring(res.content)
+        primary_dept_nodes = tree.cssselect('ul#menu li.tab')
+
+        for primary_dept_node in primary_dept_nodes:
+            primary_dept = primary_dept_node.cssselect('a div')[0].text.strip()
+            if primary_dept.lower() == 'brands':
+                continue
+
+            sub_dept = None
+            sub_dept_nodes = primary_dept_node.cssselect('div.menu ul li')
+            for sub_dept_node in sub_dept_nodes:
+                if sub_dept_node.get('class') and 'sub-heading' in sub_dept_node.get('class'):
+                    sub_dept = sub_dept_node.xpath('.//text()')[0].strip() if sub_dept_node.cssselect('a') else None
+                    continue
+
+                href_nodes = sub_dept_node.cssselect('a')
+                for href_node in href_nodes:
+                    href = href_node.get('href')
+                    key = re.search(r'(https?://shop.nordstrom.com)?/?([^\?]+)\?.*', href).groups()[1].replace('/', '_')
+                    match = re.search(r'https?://.+', href)
+                    if not match:
+                        href = '%s%s' % (HOST, href)
+
+                    title = href_node.text
+                    depts = [primary_dept]
+                    if sub_dept:
+                        depts.append(sub_dept)
+                    depts.append(title)
+
+                    is_new = False; is_updated = False
+                    category = Category.objects(key=key).first()
+
+                    if not category:
+                        is_new = True
+                        category = Category(key=key)
+
+                    if depts:
+                        for dept in depts:
+                            if dept not in category.cats:
+                                category.cats.append(dept)
+                                is_updated = True
+
+                    if href and ((category.combine_url and href.split('?')[0] != category.combine_url.split('?')[0]) \
+                        or not category.combine_url):
+                            category.combine_url = href
+                            is_updated = True
+
+                    category.hit_time = datetime.utcnow()
+                    category.save()
+                    
+                    common_saved.send(sender=ctx, obj_type='Category', key=category.key, url=category.combine_url, \
+                        is_new=is_new, is_updated=((not is_new) and is_updated) )
+
+    def crawl_listing(self, url, ctx='', **kwargs):
+        if url.startswith('http://blogs.nordstrom.com'):
+            return
+        try:
+            res = requests.get(url + '&sort=sale')
+        except requests.exceptions.ConnectionError:
+            return
+
+        res.raise_for_status()
+        tree = lxml.html.fromstring(res.content)
+        listing_node = tree.cssselect('div.fashion-results')
+
+        if listing_node:
+            listing_node = listing_node[0]
+        else:
+            if tree.cssselect('div#brandsIndex'):
+                return
+
+            self.crawl_listing_of_no_leaf(tree, ctx=ctx, **kwargs)
+            return
+
+        product_nodes = listing_node.cssselect('div.row > div')
+        if not product_nodes:
+            self.crawl_listing_of_no_leaf(tree, ctx=ctx, **kwargs)
+            return
+
+        no_discount_num = 0 # sometimes no discount product occurs between the  discount ones ordered by sale.
+        for product_node in product_nodes:
+            key = product_node.get('data-style-id')
+            if not key:
+                common_failed.send(sender=ctx, url=url, reason='listing product has no key')
+                continue
+
+            try:
+                info_node = product_node.cssselect('div.info')[0]
+                a_node = info_node.cssselect('a')[0]
+                title = a_node.text.strip()
+
+                price = None; listprice = None
+                price_nodes = info_node.cssselect(".price")
+                for price_node in price_nodes:
+                    if 'regular' in price_node.get('class'):
+                        listprice = price_node.text
+                    elif 'sale' in price_node.get('class'):
+                        price = price_node.text
+                
+                if price is None or listprice is None:
+                    no_discount_num += 1
+                    if no_discount_num < 3:
+                        continue
+                    common_failed.send(sender=ctx, url=url, \
+                        reason='listing product %s.%s cannot crawl price info -> %s / %s' % (key, title, price, listprice))
+                    return
+
+                combine_url = a_node.get('href')
+                if not combine_url:
+                    common_failed.send(sender=ctx, url=url, reason='listing product %s.%s cannot crawl combine_url' % (key, title))
+                    continue
+
+                match = re.search(r'https?://.+', combine_url)
+                if not match:
+                    combine_url = 'http://shop.nordstrom.com%s' % (combine_url)
+
+            except IndexError:
+                print traceback.format_exc()
+                common_failed.send(sender=ctx, url=url, reason='listing product %s -> %s' % (key, traceback.format_exc()))
+                continue
+
+
+            is_new = False; is_updated = False
+            product = Product.objects(key=key).first()
+            if not product:
+                is_new = True
+                product = Product(key=key)
+
+            if combine_url and combine_url != product.combine_url:
+                product.combine_url = combine_url
+                is_updated = True
+
+            if title and title != product.title:
+                product.title = title
+                is_updated = True
+
+            if price and price != product.price:
+                product.price = price
+                is_updated = True
+
+            if listprice and listprice != product.listprice:
+                product.listprice = listprice
+                is_updated = True
+
+            category = Category.objects(key=kwargs.get('key')).first()
+            if category.cats and set(category.cats).difference(product.dept):
+                product.dept = list(set(category.cats) | set(product.dept or []))
+                is_updated = True
+
+            if category.key not in product.category_key:
+                product.category_key.append(category.key)
+                is_updated = True
+
+            if is_updated:
+                product.list_update_time = datetime.utcnow()
+            
+            product.hit_time = datetime.utcnow()
+            product.save()
+            
+            print product.title
+            print product.combine_url
+            print product.listprice
+            print product.price
+            print is_new
+            print is_updated
+            print
+
+            common_saved.send(sender=ctx, obj_type='Product', key=product.key, url=product.combine_url, \
+                is_new=is_new, is_updated=((not is_new) and is_updated) )
+
+        # Go to the next page to keep on crawling.
+        try:
+            arrow_node = tree.cssselect('div.fashion-results-header div.fashion-results-pager ul.arrows li.next')[0]
+        except IndexError:
+            common_failed.send(sender=ctx, url=url, reason=traceback.format_exc())
+            return
+        next_page = arrow_node.cssselect('a')[0].get('href') \
+            if 'disabled' not in arrow_node.get('class') else None
+
+        if next_page:
+            print next_page
+            self.crawl_listing(url=next_page, ctx=ctx, **kwargs)
+
+    def crawl_product(self, url, ctx='', **kwargs):
+        key = kwargs.get('key')
+        product = Product.objects(key=key).first()
+
+        if not product:
+            common_failed.send(sender=ctx, url=url, reason='product not exists -> %s' % kwargs)
+            return
+
+        res = requests.get(url)
+        res.raise_for_status()
+        tree = lxml.html.fromstring(res.content)
+
+        breadcrumbs_nodes = tree.cssselect('ul.breadcrumbs li a')
+        depts = [breadcrumbs_node.text.strip() for breadcrumbs_node in breadcrumbs_nodes[1:]] \
+            if len(breadcrumbs_nodes) > 1 else []
+
+        product_node = tree.cssselect('div.product-content')[0]
+        
+        thumbnail_nodes = product_node.cssselect('ul.thumbnails li.fashion-photo img')
+        image_urls = [re.sub('Mini', 'Large', thumbnail_node.get('src')) for thumbnail_node in thumbnail_nodes]
+
+        brand_node = product_node.cssselect('div.brand-content ul li a')
+        brand = brand_node[0].text.strip() if brand_node else ''
+
+        title_node = product_node.cssselect('div.rightcol h1')
+        title = title_node[0].text.strip() if title_node else ''
+
+        listprice = None; price = None
+        price_nodes = product_node.cssselect('div#itemNumberPrice ul.itemNumberPriceRow li.price span.price')
+        for price_node in price_nodes:
+            if 'regular' in price_node.get('class'):
+                listprice = price_node.text.strip()
+            if 'sale' in price_node.get('class'):
+                price = price_node.text.strip()
+
+
+        shipping = None; returns = None
+        ship_and_ret_node = product_node.cssselect('div.shippingDisplay .description')
+        if  ship_and_ret_node:
+            shipping = ship_and_ret_node[0].text.strip()
+            returns = shipping
+
+        list_info = []
+        descs = product_node.cssselect('div#productdetails #pdList')[0].xpath('.//text()')
+        for desc in descs:
+            d = re.sub(r'\r|\n|\t', '', desc)
+            if d:
+                list_info.append(d.strip())
+
+        # update product
+        is_new = False; is_updated = False; ready = False
+
+        if depts and set(depts).difference(product.dept or []):
+            product.dept = list(set(depts) | set(product.dept or []))
+            is_updated = True
+
+        if image_urls and image_urls != product.image_urls:
+            product.image_urls = image_urls
+            is_updated = True
+
+        if brand and brand != product.brand:
+            product.brand = brand
+            is_updated = True
+
+        if title and not product.title:
+            product.title = title
+            is_updated = True
+
+        if listprice and listprice != product.listprice:
+            product.listprice = listprice
+            is_updated = True
+
+        if price and price != product.price:
+            product.price = price
+            is_updated = True
+
+        if shipping and shipping != product.shipping:
+            product.shipping = shipping
+            is_updated = True
+
+        if returns and returns != product.returned:
+            product.returned = returns
+            is_updated = True
+
+        if list_info and list_info != product.list_info:
+            product.list_info = list_info
+            is_updated = True
+
+        if is_updated:
+            if not product.updated:
+                # To pick the product which fit our needs, such as a certain discount, brand, dept etc.
+                selected = Picker(site='nordstrom').pick(product)
+                if not selected:
+                    # product.delete()
+                    return
+
+                ready = True
+            
+            product.updated = True
+            product.full_update_time = datetime.utcnow()
+            product.save()
+
+        common_saved.send(sender=ctx, obj_type='Product', key=product.key, url=product.combine_url, \
+            is_new=is_new, is_updated=((not is_new) and is_updated), ready=ready)
+
+        print product.dept
+        print product.image_urls
+        print product.brand
+        print product.title
+        print product.listprice
+        print product.price
+        print is_new
+        print is_updated
+        print ready
+        print product.updated
+        print
+
+
+if __name__ == '__main__':
+    # import zerorpc
+    # from settings import CRAWLER_PORT
+    # server = zerorpc.Server(Server())
+    # server.bind('tcp://0.0.0.0:{0}'.format(CRAWLER_PORT))
+    # server.run()
+
+    s = Server()
+    s.crawl_category()
+
+    counter = 0
+    categories = Category.objects()
+    for category in categories:
+        counter += 1
+        print '~~~~~~~~~~', counter
+        print category.combine_url; print
+        s.crawl_listing(category.combine_url, **{'key': category.key})
+
+        #style 1 auxpage
+        # if category.combine_url == 'http://shop.nordstrom.com/c/spring-fervor?origin=topnav&cm_sp=Top Navigation-_-Women-_-Spring Trend Guide':
+        #     s.crawl_listing(category.combine_url, **{'key': category.key})
+        #     continue
+
+    # for product in Product.objects():
+    #     s.crawl_product(product.combine_url, **{'key': product.key})
--- crpc/deals/picks.py
+++ crpc/deals/picks.py
@@ -6,68 +6,71 @@ from pipelines import ProductPipeline
 import slumber
 
 DSFILTER = slumber.API(MASTIFF_HOST).dsfilter.get()
+SITEPREF = {}
+siteprefs = slumber.API('http://mastiff.favbuy.org:8001/api/v1').sitepref.get().get('objects', [])
+for sitepref in siteprefs:
+    if sitepref.get('site'):
+        SITEPREF.setdefault(sitepref.get('site'), sitepref.get('discount_threshold_adjustment'))
+
 
 @change_for_dsfilter.bind
 def refresh_dsfilter(sender, **kwargs):
     global DSFILTER
+    global SITEPREF
     DSFILTER = slumber.API(MASTIFF_HOST).dsfilter.get()
+    SITEPREF = {}
+    siteprefs = slumber.API('http://mastiff.favbuy.org:8001/api/v1').sitepref.get().get('objects', [])
+    for sitepref in siteprefs:
+        if sitepref.get('site'):
+            SITEPREF.setdefault(sitepref.get('site'), sitepref.get('discount_threshold_adjustment'))
 
 
 class Picker(object):
-	def __init__(self, site=None):
-		self.site = site
+    def __init__(self, site=None):
+        self.site = site
 
-	def pick(self, product):
-		pp = ProductPipeline(self.site, product)
-		pp.clean()
-		return pick_by_disfilter(product)
-		# temp = '%s.^_^.%s' % (product.favbuy_brand, product.favbuy_dept)
-		# return bool(DSFILTER.get(temp, {}))
-		
-		# selected = True
-		# fns = Strategy.get(self.site, Strategy.get('default'))
-
-		# for fn in fns:
-		# 	if hasattr(fn, '__call__') and not fn(product):
-		# 		selected = False
-		
-		# return selected
+    def pick(self, product):
+        pp = ProductPipeline(self.site, product)
+        pp.clean()
+        return pick_by_disfilter(product, SITEPREF.get(self.site, SITEPREF.get('ALL')) or 1)
 
 
-def pick_by_disfilter(product):
-	if not product.favbuy_price or not product.favbuy_listprice:
-		return False
+def pick_by_disfilter(product, threshold_adjustment=1):
+    if not product.favbuy_price or not product.favbuy_listprice:
+        return False
 
-	if product.favbuy_brand:
-		filter_key = '%s.^_^.%s' % (product.favbuy_brand, product.favbuy_dept) \
-			if product.favbuy_dept else '%s.^_^.ALL' % product.favbuy_brand
+    if product.favbuy_brand:
+        filter_key = '%s.^_^.%s' % (product.favbuy_brand, product.favbuy_dept) \
+            if product.favbuy_dept else '%s.^_^.ALL' % product.favbuy_brand
 
-		threhold = DSFILTER.get(filter_key, {}).get('medium', 0)
-		discount = float(product.favbuy_price) / float(product.favbuy_listprice)
-		return discount < threhold
+        threhold = DSFILTER.get(filter_key, {}).get('medium', 0)
+        discount = float(product.favbuy_price) / float(product.favbuy_listprice)
 
-	return False
+        print discount, ' compared to threshold: %s * %s = %s', (threhold, threshold_adjustment, threhold * threshold_adjustment)
+        return discount < threhold * threshold_adjustment
+
+    return False
 
 
 # Strategy = {
-# 	'default': [pick_by_disfilter],
+#   'default': [pick_by_disfilter],
 # }
 
 
 if __name__ == '__main__':
-	from crawlers.nordstrom.models import Product
-	pick_list = []
-	products = Product.objects(dept__exists=True)
-	for product in products:
-		p = Picker('nordstrom')
-		if p.pick(product):
-			pick_list.append(product)
+    from crawlers.nordstrom.models import Product
+    # pick_list = []
+    # products = Product.objects(dept__exists=True)
+    # for product in products:
+    #     p = Picker('nordstrom')
+    #     if p.pick(product):
+    #         pick_list.append(product)
 
-	for product in pick_list:
-		print product.key, product.title
-		print product.brand, product.favbuy_brand
-		print product.dept, product.favbuy_dept
-		print float(product.favbuy_price) / float(product.favbuy_listprice)
-		print product.combine_url, '\n'
+    # for product in pick_list:
+    #     print product.key, product.title
+    #     print product.brand, product.favbuy_brand
+    #     print product.dept, product.favbuy_dept
+    #     print float(product.favbuy_price) / float(product.favbuy_listprice)
+    #     print product.combine_url, '\n'
 
-	print products.count(), len(pick_list)
+    # print products.count(), len(pick_list)

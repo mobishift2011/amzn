10fac21903f7c6ed26a7c67d5b71e65115667e64
ethan <ethan@favbuy.com>
2013-03-22 12:09:51 +0800

    picks threhold adjustment

--- crpc/deals/stats.py
+++ crpc/deals/stats.py
@@ -30,7 +30,7 @@ def main(site):
             medium = DSFILTER[filter_key]['medium']
             adjustrate = SITEPREF.get(site, SITEPREF.get('ALL')) or 1
             adjustment = float(medium) * float(adjustrate)
-            f.write('%s;%s;%s;%s;%s;%s;%s;%s\n' % (combine_url, price, listprice, disount, filter_key, medium, adjustrate, adjustment))
+            f.write('%s;%s;%s;%s;%s;%s;%s;%s\n' % (combine_url.encode('utf-8'), price, listprice, disount, filter_key, medium, adjustrate, adjustment))
 
 
 

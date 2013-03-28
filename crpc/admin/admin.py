006d0a5880414cd3dce698de0106978ec86199bf
ethan <ethan@favbuy.com>
2013-03-28 15:03:30 +0800

    deal products view for admin

--- crpc/admin/admin.py
+++ crpc/admin/admin.py
@@ -906,7 +906,7 @@ class DealHandler(BaseHandler):
 
         site = self.get_argument('site')
         offset = int(self.get_argument('offset', '0'))
-        limit = int(self.get_argument('limit', '100'))
+        limit = int(self.get_argument('limit', '40'))
         m = __import__('crawlers.%s.models' % site, fromlist=['Product'])
 
         objects = m.Product.objects

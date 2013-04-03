6ecb7635afa5ea803a0f3cc85236e171ece91393
bishop <yuandeliu@favbuy.com>
2013-04-03 18:11:22 +0800

    fix bloomingdales about crawl_category

--- crpc/crawlers/bloomingdales/server.py
+++ crpc/crawlers/bloomingdales/server.py
@@ -9,6 +9,7 @@ from datetime import datetime
 
 from models import *
 from crawlers.common.stash import *
+from crawlers.common.events import common_saved
 
 req = requests.Session(prefetch=True, timeout=30, config=config, headers=headers)
 
@@ -29,20 +30,20 @@ class Server(object):
         for d in tree.cssselect('div#bl_nav_top_menu div[class^="bl_nav_top_section_navigation_options"]'):
             link = d.cssselect('a.white')[0].get('href')
             dept = d.cssselect('a img')[0].get('alt')
-            if dept.lower() == 'designers':
-                self.crawl_designers_category(dept, link)
+            if dept == 'Designers':
+                self.crawl_designers_category(dept, link, ctx)
                 continue
-            self.crawl_to_leaf_directory(self, dept, url)
+            self.crawl_to_leaf_cateogory(dept, link, ctx)
 
-    def crawl_designers_category(self, dept, url):
+    def crawl_designers_category(self, dept, url, ctx):
         ret = self.fetch_page(url)
         base_url = re.compile('blm_insecureServer.put\("(.+)"\);').search(ret).group(1)
         pattern_url = re.compile('blm_nav_linkURL.put\("(.+)"\);').search(ret).group(1)
         for sub_dept in re.findall('blm_nav_items.put\({topCategory : "(.+?)",', ret):
             link = '{0}{1}'.format(base_url, pattern_url.replace('designerCat2', sub_dept).replace('designerCat', sub_dept))
-            self.crawl_designer_category(dept, sub_dept, link)
+            self.crawl_designer_category(dept, sub_dept, link, ctx)
 
-    def crawl_designer_category(self, dept, sub_dept, url):
+    def crawl_designer_category(self, dept, sub_dept, url, ctx):
         ret = self.fetch_page(url)
         tree = lxml.html.fromstring(ret)
         for designer in tree.cssselect('div#se_localContentContainer div.se_designerColumn li.se_designerColumn a'):
@@ -50,24 +51,39 @@ class Server(object):
             link = link if link.startswith('http') else self.siteurl + link
             brand = designer.text_content().strip()
 
-            is_new = is_updated = False
-            category = Category.objects(key=brand).first()
-            if not category:
-                is_new = True
-                categroy = Category(key=brand)
-                category.is_leaf = True
-                category.cats = [dept, sub_dept, brand]
-            category.combine_url = link + '&resultsPerPage=96'
-            category.update_time = datetime.utcnow()
-            category.save()
-            common_saved.send(sender=ctx, obj_type='Category', key=brand, url=link, is_new=is_new, is_updated=is_updated)
+            self.save_category_db([dept, sub_dept, brand], brand, link + '&resultsPerPage=96', ctx)
 
-    def crawl_to_leaf_cateogory(self, dept, url):
+
+    def crawl_to_leaf_cateogory(self, dept, url, ctx):
         ret = self.fetch_page(url)
         tree = lxml.html.fromstring(ret)
         for nav in tree.cssselect('div#gn_left_nav_container div.gn_left_nav_section'):
-            sub_dep = nav.cssselect('div.gn_left_nav_top')[0].text_content().strip()
+            sub_dept = nav.cssselect('div.gn_left_nav_top')[0].text_content().strip()
+            if sub_dept == 'FASHION INDEX':
+                continue
+            if sub_dept == 'Sale':
+                link = nav.cssselect('div.gn_left_nav_top a.gn_left_nav_sale')[0].get('href')
+                self.save_category_db([dept, sub_dept], dept + '_' + sub_dept, link, ctx)
+            else:
+                for i in nav.cssselect('ul li.gn_left_nav2_standard a.gn_left_nav'):
+                    link = i.get('href')
+                    key = link.rsplit('=', 1)[-1]
+                    sub_sub_dept = i.text_content().strip()
+                    self.save_category_db([dept, sub_dept, sub_sub_dept], key, link, ctx)
+                
 
+    def save_category_db(self, cates, key, url, ctx):
+        is_new = is_updated = False
+        category = Category.objects(key=key).first()
+        if not category:
+            is_new = True
+            category = Category(key=key)
+            category.is_leaf = True
+            category.cats = cates
+        category.combine_url = url
+        category.update_time = datetime.utcnow()
+        category.save()
+        common_saved.send(sender=ctx, obj_type='Category', key=key, url=url, is_new=is_new, is_updated=is_updated)
 
     def crawl_listing(self, url, ctx='', **kwargs):
         pass

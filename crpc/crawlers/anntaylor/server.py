7723a2c3ea8fe65b84e20160bc11d20822f508b3
bishop <yuandeliu@favbuy.com>
2013-04-18 17:47:14 +0800

    add anntaylor

--- crpc/crawlers/anntaylor/server.py
+++ crpc/crawlers/anntaylor/server.py
@@ -16,7 +16,18 @@ class Server(object):
         ret = requests.get(self.siteurl)
         tree = lxml.html.fromstring(ret.content)
         for node in tree.cssselect('div#main-hd div#nav-site ul.list-l1 li'):
-            node.cssselect('')
+            dept_name = node.cssselect('div.changeCategorys')[0].text_content().replace('AT', '').strip()
+            for node_l2 in node.cssselect('div.wrapper-l2 ul.list-l2 li'):
+                link = node_l2.cssselect('a')[0].get('href')
+                name = node_l2.cssselect('a')[0].text_content().replace('AT', '').strip()
+
+                is_new = is_updated = False
+                category = Category.objects(key=link.rsplit('/', 1)).first()
+                if not category:
+                    is_new = True
+                    category = Category(key = link.rsplit('/', 1))
+                    category.is_leaf = True
+
 
 
     def crawl_listing(self, url, ctx='', **kwargs);
